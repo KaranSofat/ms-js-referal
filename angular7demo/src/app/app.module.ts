5faf80befbd422716625154d5e96bdeb81fe1572
karan <karan.kumar@cloudsmartz.net>
2019-01-18 10:37:38 +0530

    add coming soon module

---
+++ angular7demo/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ComingsoonComponent } from './comingsoon/comingsoon.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'comingsoon',
+    component: ComingsoonComponent,
+  },
+   
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComingsoonComponent
+  ],
+  imports: [
+    BrowserModule,
+     RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: false } // <-- debugging purposes only
+  )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular7demo/src/app/comingsoon/comingsoon.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
+import 'src/assets/js/countdowntime.js';
+@Component({
+  selector: 'app-comingsoon',
+  templateUrl: './comingsoon.component.html',
+  styleUrls: ['./comingsoon.component.css']
+})
+export class ComingsoonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  $('.cd100').countdown100({
+			endtimeYear: 0,
+			endtimeMonth: 0,
+			endtimeDate: 35,
+			endtimeHours: 18,
+			endtimeMinutes: 0,
+			endtimeSeconds: 0,
+			timeZone: "" 
+		});
+  }
+
+}
